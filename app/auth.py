f747992f333ed201b978ac75ecb65085f95fcd7c
sabatierb <sabatierb@uni.coventry.ac.uk>
2023-03-30 21:20:47 +0100

    Login page and session managment

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, url_for, redirect, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 from .models import User
 from . import db
 
@@ -16,20 +16,22 @@ def login():
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
-    #remember = True if request.form.get('remember') else False
+    remember = True if request.form.get('remember') else False
 
-    user = User.query.filter_by(email=email).first()
+    user = User.query.filter_by(email=email).first()#Check if user exists
 
+    #check supplied password(-> Hashed) against password in DB(Hashed)
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login'))
 
-    login_user(user)
-    return redirect(url_for('main.account'))
+    #If check above passes, user has entred correct info
+    login_user(user, remember=remember)
+    return redirect(url_for('main.index'))
 
 
 
-#------------SIGNUP--------------#
+#***------------SIGNUP---------------------------------****#
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
@@ -55,12 +57,14 @@ def signup_post():
     db.session.add(new_user)#Add new user to users table
     db.session.commit()#Commit changes to DB
 
-    db.logger.info("New user created: {}".format(email))
+    #app.logger.info("New user created: {}".format(email))
     return redirect(url_for('auth.login'))
 
 
 
 #--------------LOGOUT-------------------#
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('auth.login'))
--- app/main.py
+++ app/main.py
@@ -1,16 +1,47 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
 from . import db
 
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
+@login_required
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name=current_user.name)
+
+
+
+#-----ROUTE TO ACCESS A MODULE------#
+@main.route('/module')#First we load the posts for the module
+@login_required
+def module():
+    module_code = request.args.get('module')#Get module code from URL
+    type = request.args.get('type')#Get type eg. Community, Journey(materials), Journey(assignments)
+
+    #Check if user is enrolled in the module
+    #IF yes load the community page
+    #ELSE dont load the page
+
+    
+    if type == 'materials':#?module=X&type=materials
+        materials = "INSERT MATERIALS HERE"
+        return render_template('index.html',name=current_user.name, code=module_code, content=materials)
+    #Get the posts for that module ?module=X
+    elif type == 'assignments':#?module=X&type=assignments
+        assignments = "INSERT ASSIGNMENTS HERE"
+        return render_template('index.html', name=current_user.name, code=module_code, content=assignments)
+    else:#?module=X&type=community or none
+        posts = "INSERT POSTS HERE"
+        return render_template('index.html', name=current_user.name, code=module_code, content=posts)
+
+
+
 
 @main.route('/account')
+@login_required
 def account():
-    return render_template('user_preferences.html')
+    return render_template('user_preferences.html', name=current_user.name)
 
 '''Function to initaliase the Database'''
 @main.route('/initdb')
