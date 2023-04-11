2c086c4c9ec0e9ceb579ca7ab039a265db9b1d48
Ben Sabatier (sabatierb) <sabatierb@coventry.ac.uk>
2023-04-11 17:02:08 +0100

    Create Module Manage Panel

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from . import db
 from .models import *
 from sqlalchemy import select
-
+from functools import wraps
 
 main = Blueprint('main', __name__)
+
+#Define a decorator to check for educator role
+def educator_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if current_user.userRole == "educator":
+            return f(*args, **kwargs)
+        else:
+            flash("You need to be an admin to view this page")
+            return redirect(url_for('main.index'))
+    return wrap
+
 
 @main.route('/')
 @login_required
@@ -106,6 +118,21 @@ def module_assignments():
         content = "Assignments"
         return render_template('index.html', code=module_code, content=content, enrolled_modules= enrolled_modules)
 
+
+
+#Route for educator to manage module
+@main.route('/module/manage')
+@login_required
+@educator_required
+def module_manage():
+    enrolled_modules= []
+    query = select(Enrolment).where(Enrolment.userId == current_user.id)
+    enrolment = db.session.execute(query)
+    for x in enrolment:
+        for y in x:
+            enrolled_modules.append(y.moduleCode)
+    module_code = request.args.get('module')
+    return render_template('manage_module.html', module_code=module_code, enrolled_modules=enrolled_modules)
 
 
 @main.route('/account')
